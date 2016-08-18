3f41b1261a2ca0e3859afd301de6d6137e7673a4
Paul Moff <moff.dev@gmail.com>
2016-08-18 21:49:12 +0700

    small changes

--- module/module.ts
+++ module/module.ts
@@ -1,17 +1,17 @@
-import { NgModule }           from '@angular/core';
-import { CommonModule }       from '@angular/common';
-import { FormsModule }        from '@angular/forms';
-import { YourAwesomeComponent }   from './your-awesome.component';
-// import { AwesomePipe }        from './awesome.pipe';
-// import { ContactService }     from './contact.service';
-// import { HighlightDirective } from './highlight.directive';
+import { NgModule }                from '@angular/core';
+import { CommonModule }            from '@angular/common';
+import { FormsModule }             from '@angular/forms';
+import { YourAwesomeComponent }    from './your-awesome.component';
+// import { YourAwesomePipe }         from './your-awesome.pipe';
+// import { YourAwesomeService }      from './your-awesome.service';
+// import { YourAwesomeDirective }    from './your-awesome.directive';
 @NgModule({
-  imports:      [ CommonModule, FormsModule ],
-  // declarations: [ ContactComponent, HighlightDirective, AwesomePipe ],
-  declarations: [ YourAwesomeComponent ],
-  // exports:      [ ContactComponent ],
-  exports:      [ YourAwesomeComponent ],
-  // providers:    [ ContactService ]
-  providers:    [ ]
+    imports:      [ CommonModule, FormsModule ],
+    // declarations: [ YourAwesomeComponent, YourAwesomeDirective, YourAwesomePipe ],
+    declarations: [ YourAwesomeComponent ],
+    // exports:      [ YourAwesomeComponent, YourAwesomeDirective, YourAwesomePipe ],
+    exports:      [ YourAwesomeComponent ],
+    // providers:    [ YourAwesomeService ]
+    providers:    []
 })
-export class YourAwesomeModule { }
+export class YourAwesomeModule {}
