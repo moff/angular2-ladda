a5087aa6b15e41dc8f43bd29a28e5b3d78b96144
Theodore Brown <theodorejb@outlook.com>
2017-12-23 20:49:01 -0600

    Minor cleanup

--- module/ladda.directive.ts
+++ module/ladda.directive.ts
@@ -60,11 +60,9 @@ export class LaddaDirective implements OnInit, OnDestroy, OnChanges {
     }
 
     ngOnDestroy() {
-        if (!this._ladda) {
-            return;
+        if (this._ladda) {
+            this._ladda.remove();
         }
-		
-		this._ladda.remove();
     }
 
     private updateLadda(previousValue: laddaValue): void {
